e55826c2d967f0d139d2d2e480c9a4863562936e
twriley25 <twrilry25@hotmail.com>
2021-11-18 14:30:36 -0500

    half finished version 2

---
+++ Frogger/Assets/Scripts/SplashManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        Vector3 destination = transform.position + left;
+        StartCoroutine(MoveText(destination));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator waitToMoveScreen()
+    {
+
+    }
+
+    private IEnumerator MoveText(Vector3 destination)
+    {
+        Vector3 startPosition = transform.position;
+        float elapsed = 0f;
+        float duration = 0.125f;
+
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+            transform.position = Vector3.Lerp(startPosition, destination, t);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = destination;
+    }
+
+}
